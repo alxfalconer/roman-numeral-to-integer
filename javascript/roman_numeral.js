5d162b97836e5d7c8236995d1a042df0c141e704
Alexander <al.x.falconer@gmail.com>
2022-05-04 14:00:00 +0100

    1st commit

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,24 @@
 function romanNumeral(string) {
   // type your code here
+  if (typeof num !== "number") {
+    return false;
+  }
+
+  var roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
 }
 
 if (require.main === module) {
